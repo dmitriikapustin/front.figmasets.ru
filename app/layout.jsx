5b582b85cb84a4853687255ecdb754aa8e46805d
vvnikita74 <124995385+vvnikita74@users.noreply.github.com>
2024-01-24 17:47:19 +0300

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,6 +5,9 @@ export const metadata = {
   description: 'GPT powered client by @dmitriikapustin',
 }
 
+export const runtime = 'edge';
+
+
 export default function RootLayout({ children }) {
   return (
     <html lang="ru" data-color-mode="dark">
