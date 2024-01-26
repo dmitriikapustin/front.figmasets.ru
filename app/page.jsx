6dfe7026cf052f7dcc07f509508154ced9c2a634
dmitriikapustin <dmitriikapustin@ya.ru>
2024-01-26 12:44:36 +0300

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -15,7 +15,7 @@ export default function Home() {
   const [messages, setMessages] = useState([]);
   const [temp, setTemp] = useState('0.5');
   const [tokens, setTokens] = useState('512');
-  const [preset, setPreset] = useState('Стандартный');
+  const [preset, setPreset] = useState('gpt-3.5-turbo');
   const [parent, setParent] = useState('0');
   const [context, setContext] = useState(false);
 
@@ -133,8 +133,8 @@ export default function Home() {
               </div>
               <div className='flex flex-col gap-2'>
                 {/* <p>Выбор температуры</p> */}
-                <Dropdown def={6} label='Температура:' options={['0.1','0.2','0.3','0.4','0.5','0.6','0.7','0.8','0.9','1']} onSelect={handleTemp} />
-                <Dropdown def={3} label='Максимум токенов: ' options={['64','128','256','512','1024','2048',]} onSelect={handleTokens} />
+                <Dropdown def={4} label='Температура:' options={['0.1','0.2','0.3','0.4','0.5','0.6','0.7','0.8','0.9','1']} onSelect={handleTemp} />
+                <Dropdown def={4} label='Максимум токенов: ' options={['64','128','256','512','1024','2048',]} onSelect={handleTokens} />
               </div>
             </div>
           </div>
